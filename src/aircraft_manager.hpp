a85d84924d6274a87493f4e0316a7aac630fcca2
scowlingmask419 <ajaafri1017@gmail.com>
2022-04-06 22:02:10 +0200

    First commit

--- src/aircraft_manager.hpp
+++ src/aircraft_manager.hpp
@@ -11,23 +11,14 @@ class Aircraft;
 class AircraftManager : public GL::DynamicObject
 {
 public:
-    AircraftManager();                     
+    // TASK_1 - B.2)
+    AircraftManager();
     ~AircraftManager() override = default;
     AircraftManager(const AircraftManager &) = delete;
-    AircraftManager &operator=(const AircraftManager &) = delete;
 
     void add_aircraft(std::unique_ptr<Aircraft>);
-    void move(double) override;
-    /*
-    unsigned count_aircraft_on_airline(const std::string_view &);
-    unsigned get_required_fuel();
-    void display_crash_number() const;
-    */
+    bool move() override;
 
 private:
     std::vector<std::unique_ptr<Aircraft>> aircrafts;
-    //unsigned crash_count = 0;
-
-    //[[maybe_unused]] void display_aircrafts();
-    //bool move_aircraft(double dt, Aircraft &craft);
 };
